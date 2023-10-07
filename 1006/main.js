58f01be29e3cf7cf2ab5f397b8f5bc0841f23836
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-10-07 13:17:10 +0900

    button, panel 갯수 5개로 변경

--- 1006/main.js
+++ 1006/main.js
@@ -4,21 +4,10 @@ const boxs = tab.querySelectorAll('section article');
 
 btns.forEach((btn, idx) => {
 	btn.addEventListener('click', () => {
-		console.log(idx);
-		/*
 		for (const el of btns) el.classList.remove('on');
 		btns[idx].classList.add('on');
 
 		for (const el of boxs) el.classList.remove('on');
 		boxs[idx].classList.add('on');
-    */
-
-		activation(btns, idx);
-		activation(boxs, idx);
 	});
 });
-
-function activation(arrEl, index) {
-	for (const el of arrEl) el.classList.remove('on');
-	arrEl[index].classList.add('on');
-}
