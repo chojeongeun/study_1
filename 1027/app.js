a20b25250088059077d50841ce4b22a2a0bf8e05
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-10-28 10:14:59 +0900

    클릭시 innerText변경

--- 1027/app.js
+++ 1027/app.js
@@ -9,4 +9,10 @@ function 색깔_바꿔주는_함수() {
 		timeElement.style.color = 'orange';
 	}
 }
-timeElement.addEventListener('click', 색깔_바꿔주는_함수);
+//timeElement.addEventListener('click', 색깔_바꿔주는_함수);
+function 숫자를_바꿔주는_함수() {
+	timeElement.innerText = '12:00';
+}
+timeElement.addEventListener('click', 숫자를_바꿔주는_함수);
+//너가 클릭했을때 실행될 함수는 이거야! 라고 이름만 쓰기
+//()를 넣어버리면 그 자리에서 실행되므로 안됨!
