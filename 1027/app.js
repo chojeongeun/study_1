ae53f949951f84035b4d62b05682b3a84d44cf55
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-10-28 10:25:21 +0900

    setTimeout과 setInterval

--- 1027/app.js
+++ 1027/app.js
@@ -1,23 +1,9 @@
-const timeElement = document.querySelector('.time');
-function 실행될_함수() {
-	timeElement.style.color = 'red';
+function sayHello() {
+	console.log('안녕하세요!!');
 }
-function 색깔_바꿔주는_함수() {
-	if (timeElement.style.color === 'orange') {
-		timeElement.style.color = 'blue';
-	} else {
-		timeElement.style.color = 'orange';
-	}
-}
-//timeElement.addEventListener('click', 색깔_바꿔주는_함수);
-function 숫자를_바꿔주는_함수() {
-	timeElement.innerText = '12:00';
-}
-timeElement.addEventListener('click', 숫자를_바꿔주는_함수);
-//너가 클릭했을때 실행될 함수는 이거야! 라고 이름만 쓰기
-//()를 넣어버리면 그 자리에서 실행되므로 안됨!
 
-timeElement.addEventListener('mouseover', 실행될_함수);
+//1회성
+//setTimeout(sayHello, 1000);
 
-//윈도우 자체에도 이벤트 줄수있음
-window.addEventListener('resize', 실행될_함수);
+//주기성
+setInterval(sayHello, 1000);
