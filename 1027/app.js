8e6571fe10da04684cd5d14cebf86c02b33027fd
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-10-29 11:24:04 +0900

    padStart 함수 사용법

--- 1027/app.js
+++ 1027/app.js
@@ -1,9 +1,14 @@
-function setTime() {
-	const time = new Date();
-	const 분 = time.getMinutes();
-	const 초 = time.getSeconds();
-	const timeH1 = document.querySelector('.time');
-	timeH1.innerText = `${분}:${초}`;
-}
+// function setTime() {
+// 	const time = new Date();
+// 	const 분 = time.getMinutes();
+// 	const 초 = time.getSeconds();
+// 	const timeH1 = document.querySelector('.time');
+// 	timeH1.innerText = `${분}:${초}`;
+// }
 
-setInterval(setTime, 1000);
+// setInterval(setTime, 1000);
+
+//padStart() 사용법
+console.log('1'.padStart(2, '0'));
+console.log('9'.padStart(5, '12345'));
+console.log('11'.padStart(2, '0'));
