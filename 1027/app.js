5b66032cab9dea3338e30e84a488b6a38b988d6a
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-10-29 11:25:51 +0900

    타이머 만들기

--- 1027/app.js
+++ 1027/app.js
@@ -1,17 +1,14 @@
+//타이머 만들기
+const 시작_시간 = new Date();
+
 function setTime() {
-	const time = new Date();
-	const 분 = time.getMinutes().toString(); //문자열로 바꿔주는 함수
-	const 초 = time.getSeconds().toString();
+	const 현재_시간 = new Date();
+	const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+
+	const 분 = 흐른_시간.getMinutes().toString().padStart(2, '0'); //문자열로 바꿔주는 함수
+	const 초 = 흐른_시간.getSeconds().toString().padStart(2, '0');
 	const timeH1 = document.querySelector('.time');
-	timeH1.innerText = `${분.padStart(2, '0')}:${초.padStart(2, '0')}`;
+	timeH1.innerText = `${분}:${초}`;
 }
 
 setInterval(setTime, 1000);
-
-//padStart() 사용법
-console.log('1'.padStart(2, '0'));
-console.log('9'.padStart(5, '12345'));
-console.log('11'.padStart(2, '0'));
-
-//padStart는 문자열 내장함수이므로 숫자는 문자열로 바꿔주기
-new Date().getMinutes.toString().padStart(2, '0');
