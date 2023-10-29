ff94cecf27e7063e0098cd5fc19553eb29f6f139
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-10-29 11:25:13 +0900

    padStart를 활용한 문제점 해결

--- 1027/app.js
+++ 1027/app.js
@@ -1,14 +1,17 @@
-// function setTime() {
-// 	const time = new Date();
-// 	const 분 = time.getMinutes();
-// 	const 초 = time.getSeconds();
-// 	const timeH1 = document.querySelector('.time');
-// 	timeH1.innerText = `${분}:${초}`;
-// }
+function setTime() {
+	const time = new Date();
+	const 분 = time.getMinutes().toString(); //문자열로 바꿔주는 함수
+	const 초 = time.getSeconds().toString();
+	const timeH1 = document.querySelector('.time');
+	timeH1.innerText = `${분.padStart(2, '0')}:${초.padStart(2, '0')}`;
+}
 
-// setInterval(setTime, 1000);
+setInterval(setTime, 1000);
 
 //padStart() 사용법
 console.log('1'.padStart(2, '0'));
 console.log('9'.padStart(5, '12345'));
 console.log('11'.padStart(2, '0'));
+
+//padStart는 문자열 내장함수이므로 숫자는 문자열로 바꿔주기
+new Date().getMinutes.toString().padStart(2, '0');
