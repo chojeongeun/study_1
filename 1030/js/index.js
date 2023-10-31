cbd2ff9142e615a8351b528ced515831ce4a9cef
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-10-31 18:55:05 +0900

    워들게임 - 특정 키를 뽑아 그 키를 현재 위치에다가 업데이트

--- 1030/js/index.js
+++ 1030/js/index.js
@@ -1,5 +1,4 @@
 /*
-
 요구사항
 1) 5글자
 2) 6번의 시도 가능
@@ -9,6 +8,18 @@
 
 (선택)키보드에도 동일하게 표시
 (선택)키보드 클릭으로도 입력
-
-
 */
+// addeventlistener안에 있는 함수는 이벤트가 암묵적으로 전달된다(event)
+
+function appStart() {
+	const handleKeydown = (e) => {
+		// 특정 키를 뽑아서 그 키를 현재 위치에다가 업데이트
+		const key = e.key;
+		const keyCode = e.keyCode;
+		const thisBlock = document.querySelector('.board-column[data-index="00"]');
+		thisBlock.innerText = key;
+	};
+	window.addEventListener('keydown', handleKeydown);
+}
+
+appStart();
