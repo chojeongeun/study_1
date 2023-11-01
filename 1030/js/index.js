c34ccd7ea1a1c5e85a7bdc9f4b137e4976322bc0
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-11-01 15:43:00 +0900

    입력한 글자 정답 확인 및 단어와 위치가 맞는지에 따라 background 색 변경

--- 1030/js/index.js
+++ 1030/js/index.js
@@ -8,7 +8,13 @@ function appStart() {
 		//입력된 단어 확인
 		for (let i = 0; i < 5; i++) {
 			const block = document.querySelector(`.board-column[data-index="${attempts}${i}"]`);
-			console.log(block.innerText);
+			const 입력한_글자 = block.innerText;
+			const 정답_글자 = 정답[i];
+			// console.log('입력한_글자', 입력한_글자, '정답_글자', 정답_글자);
+			if (입력한_글자 === 정답_글자) block.style.background = '#6aaa64';
+			else if (정답.includes(입력한_글자)) block.style.background = '#c9b458';
+			else block.style.background = '#787c7e';
+			block.style.color = 'white';
 		}
 	};
 
