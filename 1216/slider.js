dc9e308fa5e72d3f00df063cbb42f865dee78abb
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-12-16 00:11:14 +0900

    슬라이더 기능 구현

--- 1216/slider.js
+++ 1216/slider.js
@@ -6,5 +6,19 @@ btns.forEach((btn, idx) => {
 	btn.addEventListener('click', () => {
 		for (const el of btns) el.classList.remove('on');
 		btns[idx].classList.add('on');
+
+		new Anime(panel, {
+			prop: 'margin-left',
+			value: -100 * idx + '%',
+			duration: 500,
+		});
 	});
 });
+
+/*
+  0 -> 0% (-100 * 0)+'%'
+  1 -> -100% (-100 * 1)+'%'
+  2 -> -200% (-100 * 2)+'%'
+  3 -> -300% (-100 * 3)+'%'
+  4 -> -400% (-100 * 4)+'%'
+*/
