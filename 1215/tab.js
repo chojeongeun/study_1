a08c5fe4bf2d931fb699915c7b112e5801c0230a
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-12-15 23:06:15 +0900

    이벤트 함수 분리

--- 1215/tab.js
+++ 1215/tab.js
@@ -4,12 +4,20 @@ const boxs = tab.querySelectorAll('section article');
 
 btns.forEach((btn, idx) => {
 	btn.addEventListener('click', () => {
-		console.log(idx);
-
+		// console.log(idx);
+		/*
 		for (const el of btns) el.classList.remove('on');
 		btns[idx].classList.add('on');
 
 		for (const el of boxs) el.classList.remove('on');
 		boxs[idx].classList.add('on');
+    */
+		activation(btns, idx);
+		activation(boxs, idx);
 	});
 });
+// 이벤트 함수 분리
+function activation(arrEl, index) {
+	for (const el of arrEl) el.classList.remove('on');
+	arrEl[index].classList.add('on');
+}
