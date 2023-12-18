4d08ea41e393657f54bfbbf05753b4ebfeaa77a4
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-12-18 18:18:48 +0900

    쿠키 유무에 따라 팝업 숨김 유무 처리

--- 1217/cookiePop.js
+++ 1217/cookiePop.js
@@ -3,6 +3,12 @@ const ck = pop.querySelector('#ck');
 const btnClose = pop.querySelector('.close');
 const [btnShowCookie, btnDelCookie] = document.querySelectorAll('button');
 
+if (document.cookie.indexOf('today=done') < 0) {
+	pop.style.display = 'block';
+} else {
+	pop.style.display = 'none';
+}
+
 function setCookie(name, value, days) {
 	let today = new Date();
 	let duedate = today.getDate() + days;
@@ -10,7 +16,7 @@ function setCookie(name, value, days) {
 	document.cookie = `${name}=${value}; path=/; expires=${today.toUTCString()}`;
 }
 
-setCookie('today', 'done', 1);
+//setCookie('today','done',1)
 
 btnShowCookie.addEventListener('click', () => {
 	console.log(document.cookie);
@@ -19,4 +25,9 @@ btnShowCookie.addEventListener('click', () => {
 btnDelCookie.addEventListener('click', () => {
 	setCookie('today', 'done', 0);
 	alert('쿠키가 삭제되었습니다');
+});
+
+btnClose.addEventListener('click', () => {
+	if (ck.checked) setCookie('today', 'done', 1);
+	pop.style.display = 'none';
 });
