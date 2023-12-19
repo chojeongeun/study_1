d48a0610321ff963767fe1660fe27e679849c944
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-12-19 11:45:58 +0900

    슬라이더 prev 버튼 기능 구현

--- 1219/slider.js
+++ 1219/slider.js
@@ -13,3 +13,15 @@ next.addEventListener('click', () => {
 		},
 	});
 });
+
+prev.addEventListener('click', () => {
+	new Anime(panel, {
+		prop: 'margin-left',
+		value: '0%',
+		duration: 500,
+		callback: () => {
+			panel.prepend(panel.lastElementChild);
+			panel.style.marginLeft = '-100%';
+		},
+	});
+});
